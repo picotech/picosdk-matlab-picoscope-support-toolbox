c502d48ac00903c81ac267de885791093d59e2fc
hsmistry <hsmistry@users.noreply.github.com>
2019-04-17 13:07:13 +0100

    Add getVerticalAxisLabel() function

---
+++ getVerticalAxisLabel.m
@@ -0,0 +1,32 @@
+function [label] = getVerticalAxisLabel(unit)
+% getVerticalAxisLabel Obtain a label to use on a vertical axis.
+%   getVerticalAxisLabel(X) returns a suitable label for the vertical axis
+%   based on the unit X which should be one of the following:
+%
+%   * A - amps
+%   * mV - millivoltss
+%   * V - volts
+%
+%   Copyright © 2019 Pico Technology Ltd. All rights reserved.
+
+    validateattributes(unit, {'char'}, {'scalartext', 'nonempty'});
+    
+    switch (unit)
+        
+        case 'A'
+            label = 'Current (A)';
+            
+        case 'mV'
+            label = 'Voltage (mV)';
+            
+        case 'V'
+            label = 'Voltage (V)';
+            
+        otherwise
+            error('Picotech:PSTbx:getVerticalAxisLabelInvalidArg', ...
+                ['getVerticalAxisLabel: Invalid unit specified - use '...
+                'A (Amps), mV (millivolts) or V (Volts).']);
+    end
+
+end
+
